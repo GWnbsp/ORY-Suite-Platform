fda8ed017ff3d5bb5d1e19182e411a6d8873be76
CSDN4399 <2438205355@qq.com>
2024-11-15 19:27:49 +0800

    feat: 添加 @ory/client 依赖并在 HomeView.vue 中实现会话管理，更新 Vite 配置以支持 API 代理

--- docker/compose/vue/src/views/HomeView.vue
+++ docker/compose/vue/src/views/HomeView.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { FrontendApi, Configuration } from '@ory/client'
+
+const basePath = 'http://localhost:4000'
+const ory = new FrontendApi(
+  new Configuration({
+    basePath: 'http://localhost:4455',
+  }),
+)
+
+const apiUrl = import.meta.env.VITE_API_URL
+
+onMounted(() => {
+  ory.toSession().then(({ data }) => {
+    console.log(data)
+  })
+})
+</script>
 
 <template>
-  <main></main>
+  <div class=""></div>
 </template>
--- docker/compose/vue/vite.config.ts
+++ docker/compose/vue/vite.config.ts
@@ -7,14 +7,24 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4455',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/auth': {
+        target: 'http://localhost:4455',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
